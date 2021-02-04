407732663ce32ae6d0c4c1c4c593cabd8f519d58
Hubert <hwawrzacz.dev@gmail.com>
2021-02-04 19:31:37 +0100

    Initialize Firebase - get and save test data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  private _testCollection$: Observable<any>;
+
+  constructor(private _firestore: AngularFirestore) { }
+
+  public ngOnInit(): void {
+    this._testCollection$ = this._firestore.collection('test').valueChanges();
+    this.observeCollectionChanges();
+    this.addTestData();
+  }
+
+  private observeCollectionChanges(): void {
+    this._testCollection$
+      .pipe(
+        tap(info => console.log(info))
+      )
+      .subscribe()
+  }
+
+  private addTestData(): void {
+    this._firestore.collection('test').add({
+      firstName: 'Jimmie',
+      lastName: 'Giant',
+      age: 27
+    })
+      .then(res => this.handleResponse(res))
+      .catch(err => this.handleError(err))
+  }
+
+  private handleResponse(response: any): void {
+    console.log(response, 'potential succes');
+  }
+
+  private handleError(error: any): void {
+    console.error(error);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,23 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { firebaseConfig } from 'src/environments/firebase-config';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+
+    //Firebase
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
