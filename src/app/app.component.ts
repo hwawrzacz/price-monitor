b4fedad016d454e34fbb0ea813f9a58a379365e0
Hubert <hwawrzacz.dev@gmail.com>
2021-02-08 18:08:06 +0100

    Move test functionality to home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,40 +8,6 @@ import { tap } from 'rxjs/operators';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  private _testCollection$: Observable<any>;
-
-  constructor(private _firestore: AngularFirestore) { }
-
-  public ngOnInit(): void {
-    this._testCollection$ = this._firestore.collection('test').valueChanges();
-    this.observeCollectionChanges();
-    this.addTestData();
-  }
-
-  private observeCollectionChanges(): void {
-    this._testCollection$
-      .pipe(
-        tap(info => console.log(info))
-      )
-      .subscribe()
-  }
-
-  private addTestData(): void {
-    this._firestore.collection('test').add({
-      firstName: 'Jimmie',
-      lastName: 'Giant',
-      age: 27
-    })
-      .then(res => this.handleResponse(res))
-      .catch(err => this.handleError(err))
-  }
-
-  private handleResponse(response: any): void {
-    console.log(response, 'potential succes');
-  }
-
-  private handleError(error: any): void {
-    console.error(error);
-  }
+export class AppComponent {
+  constructor() { }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  private _testCollection$: Observable<any>;
 
-  constructor() {}
+  constructor(private _firestore: AngularFirestore) { }
 
+  public ngOnInit(): void {
+    this._testCollection$ = this._firestore.collection('test').valueChanges();
+    this.observeCollectionChanges();
+  }
+
+  private observeCollectionChanges(): void {
+    this._testCollection$
+      .pipe(
+        tap(info => console.log(info))
+      )
+      .subscribe()
+  }
+
+  private handleResponse(response: any): void {
+    console.log(response, 'potential succes');
+  }
+
+  private handleError(error: any): void {
+    console.error(error);
+  }
 }
