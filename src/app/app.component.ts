1173eb171fe514d31f65bb8accec0a48f5e95f0e
Hubert <hwawrzacz.dev@gmail.com>
2021-02-08 18:47:41 +0100

    Move home page to 'pages' folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
