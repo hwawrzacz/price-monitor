29f43827c95381f561ac0f918d258885cf125886
Hubert <hwawrzacz.dev@gmail.com>
2021-02-10 19:14:02 +0100

    Add empty price-insertion component

---
+++ src/app/pages/price-insertion/price-insertion.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-price-insertion',
+  templateUrl: './price-insertion.component.html',
+  styleUrls: ['./price-insertion.component.scss'],
+})
+export class PriceInsertionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
