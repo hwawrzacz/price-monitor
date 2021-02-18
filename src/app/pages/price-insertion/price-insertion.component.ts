6c773f447a62346fb1bbbafdb8df3005f53407a7
Hubert <hwawrzacz.dev@gmail.com>
2021-02-18 19:25:52 +0100

    Add form bindings to template
    Add stylings to price label.

--- src/app/pages/price-insertion/price-insertion.component.ts
+++ src/app/pages/price-insertion/price-insertion.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ModalController } from '@ionic/angular';
+import { Currency } from 'src/app/model/enums/currency';
+import { getUnitsStringList, Unit } from 'src/app/model/enums/unit';
+import { Product, ProductPrice } from 'src/app/model/product';
+import { ProfileSettingsService } from 'src/app/services/profile-settings.service';
 
 @Component({
   selector: 'app-price-insertion',
@@ -7,13 +12,57 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./price-insertion.component.scss'],
 })
 export class PriceInsertionComponent implements OnInit {
+  private initialName;
+  private _form: FormGroup;
 
-  constructor(private _modalController: ModalController) { }
+  get form(): FormGroup {
+    return this._form
+  }
 
-  ngOnInit() { }
+  get units(): string[] {
+    return getUnitsStringList();
+  }
+
+  get domesticCurrency(): Currency {
+    return this._settings.profileSettings$.value.currency;
+  }
+
+  constructor(
+    private _modalController: ModalController,
+    private _formBuilder: FormBuilder,
+    private _settings: ProfileSettingsService
+  ) { }
+
+  ngOnInit() {
+    this._form = this.buildForm();
+  }
+
+  private buildForm(): FormGroup {
+    return this._formBuilder.group({
+      name: [this.initialName, [Validators.required]],
+      price: [null, [Validators.required]],
+      unit: [Unit.PCS, [Validators.required]],
+      isBargain: [null, [Validators.required]]
+    })
+  }
+
+  private parseFromForm(): Product {
+    return {
+      name: this._form.get('name').value,
+      price: { [this.domesticCurrency]: this._form.get('price').value } as ProductPrice,
+      unit: this._form.get('unit').value,
+      domesticCurrency: this.domesticCurrency,
+      isBargain: false,
+      date: new Date(Date.now()),
+    } as Product;
+  }
+
+  public onInsert(): void {
+    const price = this.parseFromForm();
+    console.log(price);
+  }
 
   public onClose(): void {
     this._modalController.dismiss();
   }
-
 }
